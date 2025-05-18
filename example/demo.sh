06bcbaab89e0dc55fa37072c2c1ac59ccb3a19ff
Luka Mahrt-Smith <lukams03b@gmail.com>
2025-05-17 21:48:00 -0400

    Fixed Example

--- example/demo.sh
+++ example/demo.sh
@@ -5,17 +5,22 @@ set -e
 rm -rf demo-repo
 mkdir  demo-repo && cd demo-repo
 git init -q
-cp -r ../example/* .
+cp -r ../app.log ../build ../src .
 git add .
 git commit -qm "feat: initial commit with bad tracked files"
+
+# --- 3. Now (too late!) we add the .gitignore --------------------------------
+cp -r ../.gitignore .
+git add .gitignore
+git commit -qm "chore: add .gitignore"
 
 echo
 echo "== BEFORE git_clean =="
 git ls-files --cached
 
 # --- 2. Run git_clean.py (dry-run then real) ----------------
-python ../git_clean.py --dry-run
-python ../git_clean.py --yes
+python ../../git_clean.py --dry-run
+python ../../git_clean.py --yes
 
 echo
 echo "== AFTER git_clean =="
